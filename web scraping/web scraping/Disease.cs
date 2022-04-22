57802f91ed47d740150943cf2eddfa476f1cdbc7
nicosonzo03 <62284620+nicosonzo03@users.noreply.github.com>
2022-04-22 23:08:58 +0200

    Avanzamento progetto

---
+++ web scraping/web scraping/Disease.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace web_scraping
+{
+    public class Disease
+    {
+        public int id { get; set; }
+
+        public string nome { get; set; }
+        public string descrizione { get; set; }
+    }
+
+    public class Symptom
+    {
+        public int id { get; set; }
+        public string sintomo { get; set; }
+    }
+}
